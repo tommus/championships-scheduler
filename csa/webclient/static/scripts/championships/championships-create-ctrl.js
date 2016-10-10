e927469090481f610604acd65d3469a3be188c3c
Tomasz Dzieniak <tomasz.dzieniak@to-dev.com>
2016-10-10 21:59:36 +0200

    Update web client according to recent changes.

--- csa/webclient/static/scripts/championships/championships-create-ctrl.js
+++ csa/webclient/static/scripts/championships/championships-create-ctrl.js
@@ -3,53 +3,20 @@
 angular.module('csa')
     .controller('ChampionshipsCreateCtrl', function($scope, $location, Championships, Participates, Accounts, Teams) {
 
-        $scope.groups = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
-        $scope.players_teams_per_group = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
+        $scope.groups = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ];
         $scope.home_away = [{ 'id': 1, 'text': 'Single game only'}, { 'id': 2, 'text': 'Home and away' } ];
+        $scope.players = [];
+        $scope.teams = [];
 
         $scope.request = {
             'name': '',
             'groups': 1,
-            'player_teams_per_group': 1,
-            'participates': [],
+            'players': [],
+            'teams': [],
             'home_away': true
         };
 
         $scope.originalRequest = angular.copy($scope.request);
-
-        function getUser(id) {
-            if($scope.accounts === undefined) {
-                return;
-            }
-
-            var users = $scope.accounts.filter(function(account) {
-                return account.id == id;
-            });
-
-            return users[0];
-        }
-
-        function getTeam(id) {
-            if($scope.teams === undefined) {
-                return;
-            }
-
-            var teams = $scope.teams.filter(function(team) {
-                return team.id == id;
-            });
-
-            return teams[0];
-        }
-
-        $scope.getParticipate = function(participate) {
-            var team = getTeam(participate.team);
-            var user = getUser(participate.player);
-            return {
-                'participate': participate,
-                'team': team,
-                'user': user
-            }
-        };
 
         $scope.resetValues = function() {
             $scope.request = $scope.originalRequest;
@@ -64,7 +31,7 @@ angular.module('csa')
         });
 
         Accounts.query().then(function(accounts) {
-            $scope.accounts = accounts.data;
+            $scope.players = accounts.data;
         });
 
         Teams.query().then(function(teams) {
